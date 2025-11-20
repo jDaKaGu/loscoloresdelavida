62200e80d186f1f1a95ba795927ce670f9895929
jDaKaGu <jdakagu1402@gmail.com>
2025-11-19 21:55:02 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
   menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("open");
     nav.classList.toggle("active");
+    fetch(`/${section}.html`)
+
   });
 });
 
