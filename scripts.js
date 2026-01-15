634143a7bf6c36abbe370c5488c32c3c057f5163
jDaKaGu <jdakagu1402@gmail.com>
2026-01-15 16:23:58 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -39,3 +39,24 @@ document.getElementById('projectsNext')?.addEventListener('click', () => {
       behavior: 'smooth'
     });
   });
+ const slider = document.getElementById('projectsGrid');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener('mousedown', e => {
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+
+  slider.addEventListener('mouseleave', () => isDown = false);
+  slider.addEventListener('mouseup', () => isDown = false);
+
+  slider.addEventListener('mousemove', e => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5;
+    slider.scrollLeft = scrollLeft - walk;
+  });
